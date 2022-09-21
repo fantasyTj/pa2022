dcd703f7c131d3e2f0cc4c574514353e3de61745
tracer-ics2022 <tracer@njuics.org>
2022-09-21 17:24:45 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:24:45 up 1 day,  3:36,  1 user,  load average: 0.34, 0.36, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -183,7 +183,7 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  for(int i = 0; i < 32; i--){
+  for(int i = 0; i < 32; i++){
     printf("%d: %s\n", i, tokens[i].str);
   }
   // TODO();
