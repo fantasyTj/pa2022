0e8e2b3fdfb44b5eace655f9001e1e0698bc0865
tracer-ics2022 <tracer@njuics.org>
2022-09-26 23:01:46 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:01:46 up  6:59,  1 user,  load average: 0.10, 0.12, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -142,6 +142,7 @@ static bool make_token(char *e) {
             tokens[nr_token].type = rules[i].token_type;
             Assert(substr_len < 31, "Can't be stored in 32-bit str! (str is %.*s)\n", substr_len, substr_start);
             strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
             nr_token += 1;
           } break;
           default: break;
