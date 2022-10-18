a9c09a99657b20ea17a70ebcbaffb19dc7921f6d
tracer-ics2022 <tracer@njuics.org>
2022-10-18 17:32:36 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:32:36 up  3:47,  1 user,  load average: 0.62, 0.38, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,8 +14,56 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int d;
+  char *s;
+  size_t i = 0;
+
+  va_start(ap, fmt);
+  while(*fmt){
+    if(*fmt == '%'){
+      switch(*(fmt+1)){
+        // case '\0': 
+        case '%': out[i++] = '%'; break;
+        case 's': 
+          s = va_arg(ap, char *);
+          strcpy(&out[i], s);
+          i += strlen(s);
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          int tmp_d = d;
+          if(d < 0){
+            out[i++] = '-';
+            tmp_d = -tmp_d;
+          } 
+          char d_str[21];
+          d_str[20] = '\0';
+          int digit = 0;
+          while(1){
+            digit++;
+            d_str[20 - digit] = (tmp_d % 10) + '0';
+            if((tmp_d = tmp_d / 10)) break; 
+          }
+          strcpy(&out[i], &d_str[20 - digit]);
+          i += digit;
+          break;
+      }
+      fmt += 2;
+    }
+    else {
+      out[i++] = *fmt;
+      fmt += 1;
+    }
+  }
+      
+  va_end(ap);
+  out[i] = '\0';  
+
+  return i-1;
 }
+  
+
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
