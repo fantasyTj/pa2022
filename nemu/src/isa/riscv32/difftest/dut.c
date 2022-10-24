69caa37d3368bcfca0786868af9f5977a7e0eda1
tracer-ics2022 <tracer@njuics.org>
2022-10-25 00:29:24 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:29:24 up 15:38,  1 user,  load average: 0.46, 0.31, 0.28

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,11 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for(int idx = 0; idx < 32; idx++){
+    if(difftest_check_reg(reg_name(idx, 4), pc, ref_r->gpr[idx], gpr(idx))) continue;
+    else return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
