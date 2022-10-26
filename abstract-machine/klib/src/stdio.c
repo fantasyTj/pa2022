228a240412e2dd85f958376d4ec289af7339c2b3
tracer-ics2022 <tracer@njuics.org>
2022-10-26 23:57:00 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:57:00 up  4:24,  1 user,  load average: 0.39, 0.32, 0.28

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -21,12 +21,12 @@ static int32_t num2str_inv(char *start, int num){
   return idigit - 1;
 }
 
-// static int32_t str2num(char *numstr){
-//   size_t len = strlen(numstr);
-//   int32_t num = 0;
-//   for(size_t i = 0; i < len; i++) num = num * 10 + (numstr[i] - '0');
-//   return num;
-// }
+static int32_t str2num(char *numstr){
+  size_t len = strlen(numstr);
+  int32_t num = 0;
+  for(size_t i = 0; i < len; i++) num = num * 10 + (numstr[i] - '0');
+  return num;
+}
 
 static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap){
   if(is_str) assert(out);
@@ -36,22 +36,22 @@ static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap
   while(*fmt){
     if(idx >= n) break;
     if(*fmt == '%'){
-      // fmt+=1;
-      // putch(*fmt);
-      // fmt_info temp_info;
-      // char numstr[8];
-      // uint32_t t_idx = 0;
-      // while(*fmt >= '0' && *fmt <= '9'){
-      //   numstr[t_idx++] = *fmt;
-      //   fmt+=1;
-      // }
-      // numstr[t_idx] = '\0';
-      // temp_info.width = (t_idx == 0)?(0):(str2num(numstr));
-      // temp_info.type = *fmt;
-      // putch(temp_info.type);
-      // fmt+=1;
-      // putch(*fmt);
-      switch(*(fmt+1)){
+      fmt+=1;
+      putch(*fmt);
+      fmt_info temp_info;
+      char numstr[8];
+      uint32_t t_idx = 0;
+      while(*fmt >= '0' && *fmt <= '9'){
+        numstr[t_idx++] = *fmt;
+        fmt+=1;
+      }
+      numstr[t_idx] = '\0';
+      temp_info.width = (t_idx == 0)?(0):(str2num(numstr));
+      temp_info.type = *fmt;
+      putch(temp_info.type);
+      fmt+=1;
+      putch(*fmt);
+      switch(temp_info.type){
         // case '\0': 
         case '%':{
           if(is_str) out[idx++] = '%';
@@ -76,17 +76,16 @@ static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap
           char d_str[21];
           int32_t digit = num2str_inv(d_str, d);
           if(idx + digit > n) break;
-          // int32_t delta = temp_info.width - digit;
-          // if(delta > 0){
-          //   while(delta--){
-          //     if(is_str) out[idx++] = '0';
-          //     else{
-          //       putch('0');
-          //       idx++;
-          //     }
-          //   }
-            
-          // }
+          int32_t delta = temp_info.width - digit;
+          if(delta > 0){
+            while(delta--){
+              if(is_str) out[idx++] = '0';
+              else{
+                putch('0');
+                idx++;
+              }
+            }
+          }
           if(is_str) for(; digit >= 0; digit--) out[idx++] = d_str[digit];
           else{
             for(; digit >= 0; digit--) putch(d_str[digit]);
@@ -95,8 +94,7 @@ static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap
           break;
         }
       }
-      // putch(*fmt);
-      fmt+=2;
+      putch(*fmt);
     }else {
       if(is_str) out[idx++] = *fmt;
       else{
