69b1e6d87cb6b2512e5378119a061a14b96677b3
tracer-ics2022 <tracer@njuics.org>
2022-10-26 21:05:00 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:00 up  1:32,  1 user,  load average: 0.22, 0.37, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -45,7 +45,7 @@ static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap
         fmt++;
       }
       numstr[t_idx] = '\0';
-      temp_info.width = str2num(numstr);
+      temp_info.width = (t_idx == 0)?(0):(str2num(numstr));
       temp_info.type = *fmt;
       fmt++;
 
