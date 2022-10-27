c794b1d266f2fa707669d010e89dffa242f3aaf9
tracer-ics2022 <tracer@njuics.org>
2022-10-27 22:57:50 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:57:50 up  6:46,  1 user,  load average: 0.18, 0.20, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,19 +4,37 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  // int i;
+  // int w = inl(VGACTL_ADDR) >> 16;  // TODO: get the correct width
+  // int h = (uint16_t)inl(VGACTL_ADDR);  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t w_h = inl(VGACTL_ADDR);
+  uint16_t w = w_h >> 16;
+  uint16_t h = w_h;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = (int)w * (int)h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  int width = inl(VGACTL_ADDR) >> 16;
+  int idx = 0;
+  uint32_t *pixel = (uint32_t *)ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for(int h_idx = ctl->y; h_idx < ctl->y+ctl->h; h_idx++){
+    for(int w_idx = ctl->x; w_idx < ctl->x+ctl->w; w_idx++){
+      fb[h_idx*width + w_idx] = pixel[idx++];
+    }
   }
 }
 
