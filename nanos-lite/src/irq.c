6671d4d0c34bc89a24a88c209ec4399f502e7e3f
tracer-ics2022 <tracer@njuics.org>
2022-11-18 00:00:42 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:00:42 up  5:28,  1 user,  load average: 0.16, 0.15, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 4: {
-      printf("yield\n");
+      // printf("yield\n");
       break;
     }
     default: panic("Unhandled event ID = %d", e.event);
