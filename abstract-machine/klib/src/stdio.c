42318bb69ae84f88125aaa1401dad29c98912fe4
tracer-ics2022 <tracer@njuics.org>
2022-11-17 21:38:54 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:38:54 up  3:06,  1 user,  load average: 0.74, 0.57, 0.36

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -46,7 +46,7 @@ static int grl_vnp(bool is_str, char *out, size_t n, const char *fmt, va_list ap
   size_t idx = 0;
 
   while(*fmt){
-    putch(*fmt);
+    // putch(*fmt);
     if(idx >= n) break;
     if(*fmt == '%'){
       fmt+=1;
