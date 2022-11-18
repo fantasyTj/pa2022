ef85864a9f00ad11991720fd641a21fe73884b38
tracer-ics2022 <tracer@njuics.org>
2022-11-18 17:13:39 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:13:39 up 22:41,  1 user,  load average: 0.05, 0.21, 0.30

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,6 +19,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+#ifdef CONFIG_ETRACE
+  printf("Exception %d at epc" FMT_PADDR "\n", ANSI_FMT(NO, ANSI_FG_RED), epc);
+#endif
   cpu.csr.mepc = epc;
   cpu.csr.mcause = NO;
 
