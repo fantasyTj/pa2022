d32d266cfee498fb99bc466e11054ab58d56d30d
tracer-ics2022 <tracer@njuics.org>
2022-11-18 17:06:00 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:00 up 22:34,  1 user,  load average: 0.14, 0.28, 0.34

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -54,7 +54,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall"); // 
+  asm volatile("li a7, 1; ecall"); // 
 }
 
 bool ienabled() {
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 4: {
+    case EVENT_YIELD: {
       // printf("yield\n");
       break;
     }
