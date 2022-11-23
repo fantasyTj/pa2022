8f3b996066f40a81affe10e6112da1082a5e052c
tracer-ics2022 <tracer@njuics.org>
2022-11-23 20:22:16 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:16 up  4:23,  1 user,  load average: 0.15, 0.23, 0.30

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   // for(int i = 0; i < 32; i++){
   //   printf("reg%d is %u\n", i, c->gpr[i]);
   // }
-  // printf("mcause is %u\n", c->mcause);
+  printf("mcause is %u\n", c->mcause);
   // printf("mstatus is %u\n", c->mstatus);
   // printf("mepc is %u\n", c->mepc);
   if (user_handler) {
