8a1b3b481c5c16751c9458549534f814194b78dc
tracer-ics2022 <tracer@njuics.org>
2022-11-29 20:55:57 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:57 up  7:02,  1 user,  load average: 0.27, 0.22, 0.17

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <stdint.h>
+// #include "../../libs/libndl/include/NDL.h"
+uint32_t NDL_GetTicks();
+int NDL_Init(uint32_t flags);
+
 
 int main(){
-    printf("sizeof time_t is %d\n", sizeof(__time_t));
+    NDL_Init(0);
     int msec = 5;
-    struct timeval tv;
     while(1){
-        while(tv.tv_usec/100000 < msec) gettimeofday(&tv, NULL);
+        while(NDL_GetTicks()/100000 < msec) ;
         printf("dadida...\n");
         msec += 5;
     }
