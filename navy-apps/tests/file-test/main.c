6562866f6245c938efb3c809c53c00ba11ffc2c4
tracer-ics2022 <tracer@njuics.org>
2022-11-29 15:09:10 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:09:10 up  1:16,  1 user,  load average: 0.55, 0.45, 0.51

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -3,16 +3,19 @@
 
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
+  printf("here1\n");
   assert(fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  printf("here2\n");
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("here3\n");
     assert(n == i + 1);
   }
 
@@ -23,12 +26,14 @@ int main() {
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("here4\n");
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
+    printf("here5\n");
     assert(n == i + 1 + 1000);
   }
 
