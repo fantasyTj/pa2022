2662e8fb7a286ce1c7342a1421448361be4742b5
tracer-ics2022 <tracer@njuics.org>
2022-12-01 19:06:35 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:06:35 up 44 min,  1 user,  load average: 0.16, 0.19, 0.16

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -12,6 +12,7 @@ int main() {
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
+    printf("here1\n");
     fscanf(fp, "%d", &n);
     printf("n is %d\n", n);
     assert(n == i + 1);
