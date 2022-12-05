60f90294717306898ad95a8412cf0371a0ce697c
tracer-ics2022 <tracer@njuics.org>
2022-12-05 23:21:34 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:21:34 up  6:30,  1 user,  load average: 0.30, 0.35, 0.26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,7 +21,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   const char *p = buf;
   for(size_t i = 0; i < len; i++){
-    putch(*p++);
+    putch(*(p+i));
   }
   return len;
 }
