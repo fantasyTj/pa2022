a3533acfc2cb67f127671258b85b7e7495f1fdbe
tracer-ics2022 <tracer@njuics.org>
2022-12-05 23:15:15 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:15:15 up  6:23,  1 user,  load average: 0.55, 0.20, 0.18

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -73,14 +73,14 @@ extern char _end;
 void *_sbrk(intptr_t increment) {
   char temp_buf[40];
   sprintf(temp_buf, "incre is %d\n", increment);
-  _write(1, temp_buf, 40);
+  _write(2, temp_buf, 40);
   // return (void *)(-1);
   if(program_break == NULL){
     program_break = &_end;
     sprintf(temp_buf, "_end is %p\n", &_end);
-    _write(1, temp_buf, 40);
+    _write(2, temp_buf, 40);
     sprintf(temp_buf, "inipb is %p\n", program_break);
-    _write(1, temp_buf, 40);
+    _write(2, temp_buf, 40);
     // sprintf(temp_buf, "initialize\n");
     // _write(1, temp_buf, 40);
   }
@@ -89,7 +89,7 @@ void *_sbrk(intptr_t increment) {
   if(_syscall_(SYS_brk, (intptr_t)(program_break+increment), 0, 0) == 0){
     program_break += increment;
     sprintf(temp_buf, "new_pb is %p\n", program_break);
-    _write(1, temp_buf, 40);
+    _write(2, temp_buf, 40);
     return old;
   }else return (void *)(-1);
 }
