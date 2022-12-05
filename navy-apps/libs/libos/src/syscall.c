62d133d2994e67077c457436f79dae36a8698ea3
tracer-ics2022 <tracer@njuics.org>
2022-12-05 23:42:56 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:42:56 up  6:51,  1 user,  load average: 0.44, 0.37, 0.33

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,7 +67,7 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
-static char* program_break=NULL;
+static void* program_break=NULL;
 extern char _end;
 
 void *_sbrk(intptr_t increment) {
@@ -77,19 +77,19 @@ void *_sbrk(intptr_t increment) {
   // return (void *)(-1);
   if(program_break == NULL){
     program_break = &_end;
-    sprintf(temp_buf, "_end is %p\n", &_end);
-    _write(2, temp_buf, 40);
-    sprintf(temp_buf, "inipb is %p\n", program_break);
-    _write(2, temp_buf, 40);
+    // sprintf(temp_buf, "_end is %p\n", &_end);
+    // _write(2, temp_buf, 40);
+    // sprintf(temp_buf, "inipb is %p\n", program_break);
+    // _write(2, temp_buf, 40);
     // sprintf(temp_buf, "initialize\n");
     // _write(1, temp_buf, 40);
   }
   char *old = program_break;
   // char *new_pb = program_break + increment;
   if(_syscall_(SYS_brk, (intptr_t)(program_break+increment), 0, 0) == 0){
-    program_break += increment;
-    sprintf(temp_buf, "new_pb is %p\n", program_break);
-    _write(2, temp_buf, 40);
+    // program_break += increment;
+    // sprintf(temp_buf, "new_pb is %p\n", program_break);
+    // _write(2, temp_buf, 40);
     return old;
   }else return (void *)(-1);
 }
