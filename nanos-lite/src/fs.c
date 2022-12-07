bb9d2d35396f56a9d4bab6afafef2b430cef5c5b
tracer-ics2022 <tracer@njuics.org>
2022-12-07 11:51:49 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:51:49 up 13:58,  1 user,  load average: 0.15, 0.27, 0.30

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -142,6 +142,7 @@ size_t vfs_write(int fd, const void *buf, size_t count){
     }
   }else{ // fb_write
     file_table[fd].open_offset += count;
+    printf("size is %d, offset is %d\n", size, file_table[fd].open_offset);
     if(file_table[fd].open_offset >= size){
       file_table[fd].open_offset = 0;
       printf("gethere\n");
