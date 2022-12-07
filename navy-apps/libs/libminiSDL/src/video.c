1438675c8470c46881374f2b24efd8bb035f4b63
tracer-ics2022 <tracer@njuics.org>
2022-12-07 21:40:26 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:26 up 23:47,  1 user,  load average: 0.58, 1.29, 1.52

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -27,11 +27,13 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   uint32_t src_pos = src_x*(src->w) + src_y;
   uint32_t dst_pos = dst_x*(dst->w) + dst_y;
   uint16_t src_W = src->w, dst_W = src->w;
+  uint32_t *dst_px = dst->pixels, *src_px = src->pixels;
   for(int i = 0; i < w; i++){
     for(int j = 0; j < h; j++){
       // printf("i is %d, j is %d\n", i, j);
       // printf("before is %u\n", dst->pixels[dst_pos+j+i*dst_W]);
-      dst->pixels[dst_pos+j+i*dst_W] = src->pixels[src_pos+j+i*src_W];
+      // dst->pixels[dst_pos+j+i*dst_W] = src->pixels[src_pos+j+i*src_W];
+      dst_px[dst_pos+j+i*dst_W] = src_px[src_pos+j+i*src_W];
       // printf("after is %u\n", dst->pixels[dst_pos+j+i*dst_W]);
     }
   }
@@ -54,9 +56,11 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   }
   uint32_t pos = (dst->w)*x + y;
   uint16_t W = dst->w;
+  uint32_t *dst_px = dst->pixels;
   for(int i = 0; i < w; i++){
     for(int j = 0; j < h; j++){
-      dst->pixels[pos+j+i*W] = color;
+      // dst->pixels[pos+j+i*W] = color;
+      dst_px[pos+j+i*W] = color;
     }
   }
 }
