7a91ae21fb4dda9f698150a1e7f33a95a3d45c7d
tracer-ics2022 <tracer@njuics.org>
2022-12-07 11:48:44 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:48:44 up 13:55,  1 user,  load average: 0.33, 0.26, 0.30

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -142,7 +142,10 @@ size_t vfs_write(int fd, const void *buf, size_t count){
     }
   }else{ // fb_write
     file_table[fd].open_offset += count;
-    if(file_table[fd].open_offset >= size) file_table[fd].open_offset = 0;
+    if(file_table[fd].open_offset >= size){
+      file_table[fd].open_offset = 0;
+      printf("gethere\n");
+    }
     return fb_write(buf, open_offset, count);
   }
 }
