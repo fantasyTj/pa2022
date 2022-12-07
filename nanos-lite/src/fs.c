f0f8bf4bdd0c86045e88570cc03e22eab3515f34
tracer-ics2022 <tracer@njuics.org>
2022-12-07 11:09:05 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:09:05 up 13:16,  1 user,  load average: 0.41, 0.45, 0.36

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,6 +63,7 @@ static Finfo file_table[] __attribute__((used)) = {
 int fs_open(const char *pathname, int flags, int mode){
   int i = 3;
   while(true){
+    putch('0'+i);
     if(strcmp(file_table[i].name, "end") == 0) assert(0);
     if(strcmp(file_table[i].name, pathname) == 0) return i;
     i++;
