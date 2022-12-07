95d1faf7404e0229078280b45910d28555c21b8b
tracer-ics2022 <tracer@njuics.org>
2022-12-07 11:21:07 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:07 up 13:28,  1 user,  load average: 0.95, 0.45, 0.35

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -25,8 +25,8 @@ void render() {
   }
   char fname[256];
   sprintf(fname, path, cur);
-  slide = SDL_LoadBMP(fname);
   printf("gethere2\n");
+  slide = SDL_LoadBMP(fname);
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
