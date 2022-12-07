3f055b12e6bf47d3e46b7f0a20b14709d45e2265
tracer-ics2022 <tracer@njuics.org>
2022-12-07 16:42:16 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:42:16 up 18:49,  1 user,  load average: 0.19, 0.44, 0.52

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -113,7 +113,7 @@ const char *fd2name(int fd){
 size_t vfs_read(int fd, void *buf, size_t count){
   size_t size = file_table[fd].size, disk_offset = file_table[fd].disk_offset, open_offset = file_table[fd].open_offset;
   if(file_table[fd].read == NULL){
-    if(open_offset+count > size){
+    if(open_offset+count >= size){
       ramdisk_read(buf, disk_offset+open_offset, size-open_offset);
       file_table[fd].open_offset = 0;
       printf("size is %u, offset is %u\n", size, file_table[fd].open_offset);
@@ -133,7 +133,7 @@ size_t vfs_write(int fd, const void *buf, size_t count){
   if(file_table[fd].write == serial_write){
     return serial_write(buf, 0, count);
   }else if(file_table[fd].write == NULL){
-    if(open_offset+count>size){
+    if(open_offset+count >= size){
       ramdisk_write(buf, disk_offset+open_offset, size-open_offset);
       file_table[fd].open_offset = 0;
       return size-open_offset;
