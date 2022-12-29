e27f2504e5ff66e5966bb28a1a6b2f0e4e808ddb
tracer-ics2022 <tracer@njuics.org>
2022-12-29 22:19:12 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:12 up 1 day, 12:26,  1 user,  load average: 0.31, 0.31, 0.28

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -25,18 +25,6 @@ static void sh_prompt() {
 static void sh_handle_cmd(const char *cmd) {
   const char *exec_argv[2];
   char cmd_t[64];
-  // if(!strncmp(cmd, "/bin", 4)){
-  //   size_t slen = strlen(cmd);
-  //   strncpy(cmd_t, cmd, slen);
-  //   cmd_t[slen-1] = '\0';
-  //   exec_argv[0] = cmd_t;
-  //   exec_argv[1] = NULL;
-  //   printf("cmd is %s\n", exec_argv[0]);
-  //   execve(exec_argv[0], (char **)exec_argv, NULL);
-  // }else{
-  //   return;
-  // }
-
   size_t slen = strlen(cmd);
   strncpy(cmd_t, cmd, slen);
   cmd_t[slen-1] = '\0';
