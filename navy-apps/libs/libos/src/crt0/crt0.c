0e0f38292a0dfce2264eb1b670150280180cda6a
tracer-ics2022 <tracer@njuics.org>
2022-12-29 23:37:10 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:37:10 up 1 day, 13:44,  1 user,  load average: 0.89, 0.73, 0.71

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -21,7 +21,7 @@ void call_main(uintptr_t *args) {
   environ = empty;
   // environ = envp;
   // printf("pt is %p, argv is %p, envp is %p\n", pt, argv, envp);
-  exit(main(0, empty, empty));
-  // exit(main(1, argv, empty));
+  // exit(main(0, empty, empty));
+  exit(main(argc, argv, empty));
   assert(0);
 }
