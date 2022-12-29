635fa944b61d55d1d3eebe27b0ec3d5401176ad6
tracer-ics2022 <tracer@njuics.org>
2022-12-29 22:34:30 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:34:30 up 1 day, 12:41,  1 user,  load average: 0.14, 0.73, 0.93

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -71,15 +71,17 @@ void context_uload(PCB *_pcb, const char *filename, char *const argv[], char *co
   Area kstack = {.start = (void *)_pcb, .end = (void *)_pcb + sizeof(PCB)};
   uintptr_t entry = load_getentry(_pcb, filename);
   _pcb->cp = ucontext(NULL, kstack, (void *)entry);
-  _pcb->cp->GPRx = (uintptr_t)(load_args(heap.end, argv, envp));
+  // _pcb->cp->GPRx = (uintptr_t)(load_args(heap.end, argv, envp));
+  _pcb->cp->GPRx = (uintptr_t)heap.end;
 }
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)1);
   // context_kload(&pcb[1], hello_fun, (void *)1);
   char *empty[] =  {NULL };
-  char *argv[] = {"--skip", NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, empty);
+  // char *argv[] = {"--skip", NULL};
+  // context_uload(&pcb[1], "/bin/pal", argv, empty);
+  context_uload(&pcb[1], "/bin/pal", empty, empty);
   switch_boot_pcb();
 
   Log("Initializing processes...");
