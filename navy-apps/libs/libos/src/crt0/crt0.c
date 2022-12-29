c68afa225d08e6a03410443ad1899476d409fcec
tracer-ics2022 <tracer@njuics.org>
2022-12-29 23:25:00 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:25:00 up 1 day, 13:32,  1 user,  load average: 0.88, 0.64, 0.70

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,22 +5,22 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("args is %p\n", args);
-  void *pt = (void *)args;
-  int argc = *(int *)pt;
-  printf("argc is %d\n", argc);
-  char **argv = (char **)(pt + sizeof(int *));
-  printf("reach here1\n");
-  char **envp = argv;
-  printf("reach here2\n");
-  while(*envp) envp++;
-  envp++;
-  printf("reach here3\n");
+  // printf("args is %p\n", args);
+  // void *pt = (void *)args;
+  // int argc = *(int *)pt;
+  // printf("argc is %d\n", argc);
+  // char **argv = (char **)(pt + sizeof(int *));
+  // printf("reach here1\n");
+  // char **envp = argv;
+  // printf("reach here2\n");
+  // while(*envp) envp++;
+  // envp++;
+  // printf("reach here3\n");
   char *empty[] =  {NULL };
-  // environ = empty;
-  environ = envp;
-  printf("pt is %p, argv is %p, envp is %p\n", pt, argv, envp);
-  // exit(main(0, empty, empty));
-  exit(main(argc, argv, envp));
+  environ = empty;
+  // environ = envp;
+  // printf("pt is %p, argv is %p, envp is %p\n", pt, argv, envp);
+  exit(main(0, empty, empty));
+  // exit(main(argc, argv, envp));
   assert(0);
 }
