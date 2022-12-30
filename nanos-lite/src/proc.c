4a1a423fcf3f3351aa6af4044007e7d2de055422
tracer-ics2022 <tracer@njuics.org>
2022-12-30 20:17:50 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:17:50 up 2 days, 10:25,  1 user,  load average: 0.58, 0.54, 0.40

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -70,7 +70,7 @@ static void *load_args(void *end, char *const argv[], char *const envp[]) {
     char_start++;
     char_semi += envp_num_arr[i];
   }
-  printf("reach end\n");
+  // printf("reach end\n");
   // printf("new heap_end is %p\n", argc_pt);
   return (void *)argc_pt;
 }
