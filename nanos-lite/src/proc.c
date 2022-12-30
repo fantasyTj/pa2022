ffb1c8a156ad960272a47d520c7a8746b59755a0
tracer-ics2022 <tracer@njuics.org>
2022-12-30 19:38:09 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:38:09 up 2 days,  9:45,  1 user,  load average: 0.21, 0.33, 0.28

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -83,7 +83,9 @@ void context_uload(PCB *_pcb, const char *filename, char *const argv[], char *co
   printf("%p %p\n", envp, *envp);
   Area kstack = {.start = (void *)_pcb, .end = (void *)_pcb + sizeof(PCB)};
   uintptr_t entry = load_getentry(_pcb, filename);
+  printf("1%p %p\n", envp, *envp);
   _pcb->cp = ucontext(NULL, kstack, (void *)entry);
+  printf("2%p %p\n", envp, *envp);
   _pcb->cp->GPRx = (uintptr_t)(load_args((new_page(8)+(8*PGSIZE)), argv, envp));
   // _pcb->cp->GPRx = (uintptr_t)(load_args(heap.end, argv, envp));
   // _pcb->cp->GPRx = (uintptr_t)heap.end;
