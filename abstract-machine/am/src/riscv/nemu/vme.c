0f5594319efd4d6527c2035e54263cee2af740e8
tracer-ics2022 <tracer@njuics.org>
2023-01-01 19:31:58 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:31:58 up 4 days,  9:39,  1 user,  load average: 0.38, 0.95, 0.72

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -14,7 +14,7 @@ static Area segments[] = {      // Kernel memory mappings
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
 static inline void set_satp(void *pdir) {
-  printf("reach here1\n");
+  printf("pdir is %p, reach here1\n", pdir);
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
   printf("reach here2\n");
