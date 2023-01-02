85351e2618269ac643461dd0631314c83c361519
tracer-ics2022 <tracer@njuics.org>
2023-01-02 21:15:18 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:18 up 5 days, 11:22,  1 user,  load average: 0.28, 0.33, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -129,7 +129,6 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  printf("ini INTR is %u\n", cpu.INTR);
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
