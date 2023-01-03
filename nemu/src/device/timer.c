f361b3e2c4ab633742f3cb799aa6a93a8e8af8bc
tracer-ics2022 <tracer@njuics.org>
2023-01-03 11:09:01 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:09:01 up 6 days,  1:16,  1 user,  load average: 1.33, 0.57, 0.30

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -32,7 +32,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 static void timer_intr() {
   static uint64_t last;
   uint64_t now = get_time();
-  printf("interval is %lu\n", (now-last)/1000);
+  printf("interval is %lu\n", (now-last));
   last = now;
   // printf("call timer_intr\n");
   if (nemu_state.state == NEMU_RUNNING) {
