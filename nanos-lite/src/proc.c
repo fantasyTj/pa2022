b8749994e58412fd97c007a93524bccea9452590
tracer-ics2022 <tracer@njuics.org>
2023-01-04 10:47:59 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:47:59 up 7 days, 55 min,  1 user,  load average: 0.62, 0.43, 0.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -94,9 +94,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 void init_proc() {
   char *empty[] =  {NULL };
-  // char *argv0[] = {"/bin/hello", NULL};
-  // context_uload(&pcb[1], "/bin/hello", argv0, empty);
-  context_kload(&pcb[0], hello_fun, (void *)1);
+  char *argv0[] = {"/bin/hello", NULL};
+  context_uload(&pcb[0], "/bin/hello", argv0, empty);
+  // context_kload(&pcb[0], hello_fun, (void *)1);
   // char *argv[] = {"/bin/pal", "--skip", NULL};
   // context_uload(&pcb[0], "/bin/pal", argv, empty);
 
