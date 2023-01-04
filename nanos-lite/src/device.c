f760020910d7c68cffdda832355969d5f53c1eca
tracer-ics2022 <tracer@njuics.org>
2023-01-04 10:44:07 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:44:07 up 7 days, 51 min,  1 user,  load average: 0.46, 0.33, 0.26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,7 +50,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   if(buf == NULL) return 0;
   else{ // only support 
     AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
