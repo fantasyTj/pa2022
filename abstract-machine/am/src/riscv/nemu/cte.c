ded0953845c64d5aaa2d3844407642c26f4e8542
tracer-ics2022 <tracer@njuics.org>
2023-01-04 12:01:06 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:01:06 up 7 days,  2:08,  1 user,  load average: 1.39, 0.49, 0.22

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   __am_get_cur_as(c);
-  printf("c.np is %p, mepc is %p, sp is %p\n", c->np, c->mepc, c->gpr[2]);
+  // printf("c.np is %p, mepc is %p, sp is %p\n", c->np, c->mepc, c->gpr[2]);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -34,7 +34,7 @@ Context* __am_irq_handle(Context *c) {
     // c->mstatus = 0x1800; // due to change the context, here are some problems, we may need to handle it in function "user_handler"
     assert(c != NULL);
   }
-  printf("! c.np is %p, mepc is %p, sp is %p\n", c->np, c->mepc, c->gpr[2]);
+  // printf("! c.np is %p, mepc is %p, sp is %p\n", c->np, c->mepc, c->gpr[2]);
   if(c->np != 0) {
     asm volatile("csrw mscratch, %0" : : "r"((uintptr_t)((void *)c + sizeof(Context))));
   }else {
