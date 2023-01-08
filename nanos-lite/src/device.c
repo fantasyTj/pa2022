a4e06091d03dc88e635491f0cbb572093d7ad5d5
tracer-ics2022 <tracer@njuics.org>
2023-01-08 15:29:02 +0800

    >  compile NEMU 211300097 童锦 Linux fantasy 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:29:02 up 35 min,  1 user,  load average: 0.19, 0.83, 0.71

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,9 +39,10 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   }else{
     if(ev.keycode == AM_KEY_F1 || ev.keycode == AM_KEY_F2 || ev.keycode == AM_KEY_F3) {
       extern int proc_num;
-      if(ev.keycode == AM_KEY_F1) proc_num = 1;
-      else if(ev.keycode == AM_KEY_F2) proc_num = 2;
-      else proc_num = 3;
+      // if(ev.keycode == AM_KEY_F1) proc_num = 1;
+      // else if(ev.keycode == AM_KEY_F2) proc_num = 2;
+      // else proc_num = 3;
+      proc_num = ev.keycode;
       // exchange_proc(proc_num);
       yield();
     }
